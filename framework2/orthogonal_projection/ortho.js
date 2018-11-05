52c1adebe71f9322bd9802af83db5fdc5b7830aa
Dante Niewenhuis <d.niewenhuis@hotmail.com>
2018-11-05 16:20:57 +0100

    finished all exercises

--- framework2/orthogonal_projection/ortho.js
+++ framework2/orthogonal_projection/ortho.js
@@ -6,11 +6,14 @@
 
 function myOrtho(left, right, bottom, top, near, far) {
 
+    Nx = 2;
+    Ny = 2;
+    Nz = 2;
     var mat = [
-        1.0 ,0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
+        Nx/(right-left),    0.0,                0.0,            -(left+right)/2,
+        0.0,                Ny/(top-bottom),    0.0,            -(bottom+top)/2,
+        0.0,                0.0,                Nz/(near-far),  -(near+far)/2,
+        0.0,                0.0,                0.0,            1.0
     ];
 
     return mat;
--- framework2/positioning_the_camera/lookat.js
+++ framework2/positioning_the_camera/lookat.js
@@ -4,16 +4,74 @@
  *
  */
 
+function norm(a) {
+    var l = Math.sqrt(dot(a, a));
+
+    return vectorDivide(a, l);
+}
+
+function crossProduct(a, b) {
+    return [a[1]*b[2] - a[2]*b[1], 
+            a[2]*b[0] - a[0]*b[2], 
+            a[0]*b[1] - a[1]*b[0]];
+}
+
+function dot(a, b) {
+    var res = 0;
+    for (var i = 0; i < 3; i++) {
+        res += a[i] * b[i];
+    }
+
+    return res;
+}
+
+function vectorDivide(a, l) {
+    var res = [0,0,0];
+    for (var i = 0; i < 3; i++) {
+        res[i] = a[i] / l;
+    }
+
+    return res;
+}
+
+function findNonColinear(a) {
+    var res = [a[0], a[1], a[2]];
+    var smallestIndex = 0;
+    var smallestValue = res[0];
+    for (var i = 1; i < 3; i++) {
+        if (res[i] < smallestValue) {
+            smallestValue = res[i];
+            smallestIndex = i;
+        }
+    }
+
+    res[smallestIndex] = 1;
+
+    return res;
+}
+
 function myLookAt(eyeX, eyeY, eyeZ,
                   centerX, centerY, centerZ,
                   upX, upY, upZ) {
 
     var mat = [
-        1.0, 0.0, 0.0, -centerX,
-        0.0, 1.0, 0.0, -centerY,
-        0.0, 0.0, 1.0, -centerZ,
-        0.0, 0.0, 0.0, 1.0
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        -eyeX, -eyeY, -eyeZ, 1.0
     ];
 
-    return mat;
+    var cz = [eyeX - centerX, eyeY - centerY, eyeZ - centerZ];
+    cz = norm(cz);
+
+    var cx = norm(crossProduct([upX, upY, upZ], cz));
+
+    var cy = norm(crossProduct(cz, cx));
+
+    var R = [cx[0], cx[1], cx[2],0.0,
+             cy[0], cy[1], cy[2],0.0,
+             cz[0], cz[1], cz[2],0.0,
+            0.0, 0.0, 0.0, 1.0];
+
+    return m4.multiply(m4.transpose(R), mat);
 }
