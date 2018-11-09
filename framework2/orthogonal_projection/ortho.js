fe05a0f762ae81cae9ffcb73f27ca8e196a3b972
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-11-09 10:24:29 +0100

    transposed the matrix (still works)

--- framework2/orthogonal_projection/ortho.js
+++ framework2/orthogonal_projection/ortho.js
@@ -10,10 +10,10 @@ function myOrtho(left, right, bottom, top, near, far) {
     Ny = 2;
     Nz = 2;
     var mat = [
-        Nx/(right-left),    0.0,                0.0,            -(left+right)/2,
-        0.0,                Ny/(top-bottom),    0.0,            -(bottom+top)/2,
-        0.0,                0.0,                Nz/(near-far),  -(near+far)/2,
-        0.0,                0.0,                0.0,            1.0
+        Nx/(right-left),    0.0,                0.0,            0.0,
+        0.0,                Ny/(top-bottom),    0.0,            0.0,
+        0.0,                0.0,                Nz/(near-far),  0.0,
+        -(left+right)/2,    -(bottom+top)/2,    -(near+far)/2,  1.0
     ];
 
     return mat;
