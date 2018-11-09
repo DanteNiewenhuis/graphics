997a7025b34fad17ead656d6c1cb515119585419
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-11-09 15:26:17 +0100

    Add files via upload

--- framework2/orthogonal_projection/ortho.js
+++ framework2/orthogonal_projection/ortho.js
@@ -1,18 +1,17 @@
 /*
- * Student names:
- * Student numbers:
+ * Student name .... Dante Niewenhuis, Thomas Bellucci
+ * Student email ... th.bellucci@gmail.com & d.niewenhuis@hotmail.com
+ * Collegekaart .... 11058595, 11257245
+ * Date ............ 9 november 2018
  */
 
 function myOrtho(left, right, bottom, top, near, far) {
-
-    Nx = 2;
-    Ny = 2;
-    Nz = 2;
+	// Transform cuboid to 2x2x2 cube at the origin.
     var mat = [
-        Nx/(right-left),    0.0,                0.0,            0.0,
-        0.0,                Ny/(top-bottom),    0.0,            0.0,
-        0.0,                0.0,                Nz/(near-far),  0.0,
-        -(left+right)/2,    -(bottom+top)/2,    -(near+far)/2,  1.0
+        2/(right-left),  0.0,             0.0,           0.0,
+        0.0,             2/(top-bottom),  0.0,           0.0,
+        0.0,             0.0,             2/(near-far),  0.0,
+        -(left+right)/2, -(bottom+top)/2, -(near+far)/2, 1.0
     ];
 
     return mat;
