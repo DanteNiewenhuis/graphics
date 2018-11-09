9a9fe4253f7f0670b0a2c33ecdd01a453eb8bfb5
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-11-09 15:27:05 +0100

    Add files via upload

--- framework2/transformations/transformations.js
+++ framework2/transformations/transformations.js
@@ -1,7 +1,8 @@
 /*
- * Student names:
- * Student numbers:
- *
+ * Student name .... Dante Niewenhuis, Thomas Bellucci
+ * Student email ... th.bellucci@gmail.com & d.niewenhuis@hotmail.com
+ * Collegekaart .... 11058595, 11257245
+ * Date ............ 9 november 2018
  */
 
 function myTranslate(x, y, z) {
@@ -29,36 +30,38 @@ function myScale(x, y, z) {
 }
 
 function norm(a) {
+	// Normalizes a vector to unit length.
     var l = Math.sqrt(dot(a, a));
-
     return vectorDivide(a, l);
 }
 
 function crossProduct(a, b) {
+	// Calculates the cross-product between two vectors.
     return [a[1]*b[2] - a[2]*b[1], 
             a[2]*b[0] - a[0]*b[2], 
             a[0]*b[1] - a[1]*b[0]];
 }
 
 function dot(a, b) {
+	// Calculates the dot-product between two vectors.
     var res = 0;
     for (var i = 0; i < 3; i++) {
         res += a[i] * b[i];
     }
-
     return res;
 }
 
 function vectorDivide(a, l) {
+	// Multiplies vector with 1 / a.
     var res = [0,0,0];
     for (var i = 0; i < 3; i++) {
         res[i] = a[i] / l;
     }
-
     return res;
 }
 
 function findNonColinear(a) {
+	// Calculates a non-colinear for the vector a.
     var res = [a[0], a[1], a[2]];
     var smallestIndex = 0;
     var smallestValue = res[0];
@@ -68,19 +71,14 @@ function findNonColinear(a) {
             smallestIndex = i;
         }
     }
-
     res[smallestIndex] = 1;
-
     return res;
 }
 
 function myRotate(angle, x, y, z) {
     // Rotate by angle around [x, y, z]^T.
 
-    //
-    // 1. Create the orthonormal basis
-    //
-    
+    // Create the orthonormal basis.
     var w = norm([x, y, z]);
 
     var t = findNonColinear(w);
@@ -93,8 +91,8 @@ function myRotate(angle, x, y, z) {
 
     var v = crossProduct(w, u);
     
-    // 2. Set up the three matrices making up the rotation
-    //
+    // Create matrix out of orthonormal basis to rotate vector
+	// we want to rotate around to z-axis.
     var A = [
             u[0], u[1], u[2],0.0,
             v[0], v[1], v[2],0.0,
@@ -102,16 +100,17 @@ function myRotate(angle, x, y, z) {
             0.0,  0.0,  0.0, 1.0
             ];
 
+	// Rotates around Z-axis.
     var B = [
-             Math.cos(angle),   Math.sin(angle),   0.0, 0.0,
-            -Math.sin(angle),   Math.cos(angle),    0.0, 0.0,
-             0.0,               0.0,                1.0, 0.0,
-             0.0,               0.0,                0.0, 1.0
+             Math.cos(angle),  Math.sin(angle), 0.0, 0.0,
+             -Math.sin(angle), Math.cos(angle), 0.0, 0.0,
+             0.0,              0.0,             1.0, 0.0,
+             0.0,              0.0,             0.0, 1.0
             ];
 
     var C = m4.transpose(A);
 
-
+	// Combine all matrices into a single matrix.
     var mat = m4.multiply(A, m4.multiply(B, C));
     return mat;
 }
