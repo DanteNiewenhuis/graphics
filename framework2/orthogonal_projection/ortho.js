003aaca26e53791b0a3c12e1b0f59148ae4ed23f
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-11-09 15:33:09 +0100

    Update ortho.js

--- framework2/orthogonal_projection/ortho.js
+++ framework2/orthogonal_projection/ortho.js
@@ -6,13 +6,12 @@
  */
 
 function myOrtho(left, right, bottom, top, near, far) {
-	// Transform cuboid to 2x2x2 cube at the origin.
+    // Transform cuboid to 2x2x2 cube at the origin.
     var mat = [
         2/(right-left),  0.0,             0.0,           0.0,
         0.0,             2/(top-bottom),  0.0,           0.0,
         0.0,             0.0,             2/(near-far),  0.0,
         -(left+right)/2, -(bottom+top)/2, -(near+far)/2, 1.0
     ];
-
     return mat;
 }
