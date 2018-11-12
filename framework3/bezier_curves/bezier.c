e959b3ac3c9a9ee143273e7a959e17bcb932324b
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-11-12 15:00:43 +0100

    Add files via upload

--- framework3/bezier_curves/bezier.c
+++ framework3/bezier_curves/bezier.c
@@ -25,12 +25,42 @@
  * Return the x and y values of the point by setting *x and *y,
  * respectively.
  */
+ 
+// Calculates the factorial of some number k.
+float fac(int k) {
+    if (k) {
+        int num = k;
+        for (int i = k - 1; i > 0; i--) {
+            num *= i;
+        }
+        return (float) num;
+    }
+    return 1.0;
+}
 
+// Calculates the binomial distribution.
+float binomial_distr(int n, int k) {
+    return fac(n) / (fac(k) * fac(n - k));
+}
+
+// Evaluates a bezier curve.
 void
 evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u)
 {
     *x = 0.0;
     *y = 0.0;
+    
+    float bino_dist, B_n;
+    int n = num_points - 1;
+
+    for (int i = 0; i < num_points; i++) {
+        bino_dist = binomial_distr(n, i);
+        
+        B_n = bino_dist * pow(u, i) * pow(1 - u, n - i);
+        
+        *x += B_n * p[i].x;
+        *y += B_n * p[i].y;
+    }
 }
 
 /* Draw a Bezier curve defined by the control points in p[], which
@@ -54,18 +84,28 @@ draw_bezier_curve(int num_segments, control_point p[], int num_points)
     GLuint buffer[1];
 
     /* Write your own code to create and fill the array here. */
-
+    GLfloat verts[num_segments+1][2];
+    
+    float x, y;
+    int i;
+    float u;
+    float u_incr = 1.0 / num_segments;
+    for (i = 0, u = 0; u <= 1; i++, u += u_incr) {
+        evaluate_bezier_curve(&x, &y, p, num_points, u);
+        verts[i][0] = x;
+        verts[i][1] = y;
+    }
 
     // This creates the VBO and binds an array to it.
     glGenBuffers(1, buffer);
     glBindBuffer(GL_ARRAY_BUFFER, *buffer);
-    glBufferData(GL_ARRAY_BUFFER, 0 /* Fill in the right size here*/,
-                 NULL /*Fill in the pointer to the array*/, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
+                 verts, GL_STATIC_DRAW);
 
     // This tells OpenGL to draw what is in the buffer as a Line Strip.
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(2, GL_FLOAT, 0, 0);
-    glDrawArrays(GL_LINE_STRIP, 0, 0/* Fill in the number of steps to be drawn*/);
+    glDrawArrays(GL_LINE_STRIP, 0, num_segments+1);
     glDisableClientState(GL_VERTEX_ARRAY);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glDeleteBuffers(1, buffer);
