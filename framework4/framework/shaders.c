290bc533c6c68186b6bd82136295c368c77a8532
Dante Niewenhuis <d.niewenhuis@hotmail.com>
2018-11-21 10:58:30 +0100

    merge

--- framework4/framework/shaders.c
+++ framework4/framework/shaders.c
@@ -140,7 +140,37 @@ shade_blinn_phong(intersection_point ip)
 vec3
 shade_reflection(intersection_point ip)
 {
-    return v3_create(1, 0, 0);
+        vec3 noise;
+    
+    float I = 0, I_tmp = 0;
+    for (int i = 0; i < scene_num_lights; i++) {
+        // For each light, get its position and intensity.
+        light li = scene_lights[i];
+        
+        // Determine unit vector pointing towards the lamp.
+        vec3 l = v3_normalize(v3_subtract(li.position, ip.p)); 
+        
+        // Check whether this l vector is obstructed by an object.
+        noise = v3_multiply(l, 0.001);
+        if (shadow_check(v3_add(ip.p, noise), l)) {
+            continue;
+        }
+        
+        // Calculate intensity of surface for this one light.
+        I_tmp = max(0, v3_dotprod(ip.n, l));
+        
+        // Accumulate light over all lights.
+        I += I_tmp * li.intensity;    
+    }
+    
+    // Add ambient component
+    I += scene_ambient_light;
+    
+    // Combine with diffuse color coefficients
+    vec3 c_f = v3_multiply(v3_create(1, 1, 1), I);
+    
+    // Clip final color if values exceed 1.0
+    return clip(c_f);
 }
 
 // Returns the shaded color for the given point to shade.
